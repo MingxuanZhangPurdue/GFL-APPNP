64a85c49dbc072b36e19573bb10eeeb372242608
Mingxuan Zhang <zhan3692@purdue.edu>
2022-02-22 22:05:34 -0500

    update

--- models/GFLAPPNP_NC.py
+++ models/GFLAPPNP_NC.py
@@ -253,10 +253,6 @@ class Central_Server:
 
         train_loss, train_acc, val_loss, val_acc = self.eval_train_val()
         
-<<<<<<< Updated upstream
-        
-=======
->>>>>>> Stashed changes
         if (val_loss < self.best_valloss):
             self.best_valloss = val_loss
             self.best_cmodel = copy.deepcopy(self.cmodel)
