6742eb432c8b7be282f09b2d3a39aa92ca2028fe
MingxuanZhangPurdue <74074145+MingxuanZhangPurdue@users.noreply.github.com>
2022-08-28 17:04:41 -0400

    f

--- csbm_dnc.py
+++ csbm_dnc.py
@@ -19,10 +19,10 @@ import os
 
 parser = argparse.ArgumentParser(description='cSBM DNC experiments.')
 parser.add_argument('--Print', default=False, type=bool, help='Whether to print training stats during training.')
-parser.add_argument('--print_time', default=1, type=int, help='Print stat for each print_time communications.')
+parser.add_argument('--print_time', default=10, type=int, help='Print stat for each print_time communications.')
 
 parser.add_argument('--gradient', default=True, type=bool, help='Share gradient of hidden representation.')
-parser.add_argument('--hidden_noise', default=False, type=bool, help='Add random noise to hidden representation.')
+parser.add_argument('--hidden_noise', default=True, type=bool, help='Add random noise to hidden representation.')
 parser.add_argument('--gradient_noise', default=False, type=bool, help='Add random noise to gradient.')
 parser.add_argument('--hn_std', default=0.01, type=float, help='Standard deviation for hidden noise.')
 parser.add_argument('--gn_std', default=0.01, type=float, help='Standard deviation for gradient noise.')
