85e157b630098242054f6f294c41fbb36967f569
MingxuanZhangPurdue <74074145+MingxuanZhangPurdue@users.noreply.github.com>
2022-09-05 16:22:49 -0400

    v

--- csbm_sc.py
+++ csbm_sc.py
@@ -27,15 +27,15 @@ parser.add_argument('--nvalid', default=10, type=int, help='Valid set size.')
 parser.add_argument('--gradient', default=True, type=bool, help='Share gradient of hidden representation.')
 parser.add_argument('--batchsize', default=5, type=int, help='Batch size for local updates.')
 parser.add_argument('--hidden_noise', default=True, type=bool, help='Add random noise to hidden representation.')
-parser.add_argument('--gradient_noise', default=False, type=bool, help='Add random noise to gradient.')
+parser.add_argument('--gradient_noise', default=True, type=bool, help='Add random noise to gradient.')
 parser.add_argument('--hn_std', default=1, type=float, help='Standard deviation for hidden noise.')
-parser.add_argument('--gn_std', default=0.1, type=float, help='Standard deviation for gradient noise.')
+parser.add_argument('--gn_std', default=1, type=float, help='Standard deviation for gradient noise.')
 parser.add_argument('--bias', default=False, type=bool, help='Bias in MLP.')
 
 
-parser.add_argument('--lr', default=0.2, type=float, help='Learning rate.')
+parser.add_argument('--lr', default=0.05, type=float, help='Learning rate.')
 parser.add_argument('--I', default=10, type=int, help='Number of local updates.')
-parser.add_argument('--nc', default=200, type=int, help='Number of communications.')
+parser.add_argument('--nc', default=400, type=int, help='Number of communications.')
 
 
 args = parser.parse_args()
